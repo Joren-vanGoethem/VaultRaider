fb96b7818c485c7534dea8f550e8a905a37e2a50
jorenvangoethem <jorenvangoethem@hotmail.com>
2026-01-13 18:35:46 +0100

    fix tanstack and tailwind implementation

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
@@ -9,7 +10,11 @@ const host = process.env.TAURI_DEV_HOST;
 export default defineConfig(async () => ({
   plugins: [
     react(),
-    TanStackRouterVite(),
+    tailwindcss(),
+    tanstackRouter({
+        target: 'react',
+        autoCodeSplitting: true,
+    }),
   ],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
