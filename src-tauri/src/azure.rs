a660b6b4a6de24ecd05999bb8a108ffbfb4fd4bd
Joren vanGoethem <jorenvangoethem@hotmail.com>
2026-02-02 20:29:11 +0100

    remove backwards compatibility module

--- src-tauri/src/azure.rs
+++ src-tauri/src/azure.rs
@@ -7,10 +7,3 @@ pub(crate) mod http;
 pub(crate) mod keyvault;
 pub(crate) mod resource_group;
 pub(crate) mod subscription;
-
-// Keep old module for backwards compatibility during migration
-#[deprecated(note = "Use azure::subscription module instead")]
-pub(crate) mod subscriptions {
-    pub use super::subscription::service::get_subscriptions;
-    pub use super::subscription::types::Subscription;
-}
