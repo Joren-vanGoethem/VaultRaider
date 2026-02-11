af9ae9af1bf06bb639697859e4c14923c5cb3f75
joren.vangoethem <jorenvangoethem@hotmail.com>
2026-02-11 08:08:53 +0100

    fix responsiveness on keyvault page

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -6,6 +6,8 @@
 export { ActionButton } from "./ActionButton";
 export { Alert, EmptyStateAlert, LoadingAlert } from "./Alert";
 export { Button, IconButton } from "./Button";
+export type { DropdownMenuItem } from "./DropdownMenu";
+export { DropdownButton, DropdownMenu } from "./DropdownMenu";
 export { FormGroup, FormInput, FormLabel, FormSelect, FormTextarea } from "./FormInput";
 export type { ModalProps } from "./Modal";
 export { Modal, ModalDescription, ModalFooter, ModalTitle } from "./Modal";
